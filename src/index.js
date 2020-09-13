5ce85b492eb0b570ef4e76206e8517aff3359ec3
Daniil <sitnikovdan13@gmail.com>
2020-09-13 13:23:40 +0300

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let res = n.toString().split('').reverse();
+  if (res[res.length - 1] === '-') res.pop();
+  res = res.join('');
+  return Number(res);
 }
